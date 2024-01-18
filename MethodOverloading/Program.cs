b0e56f3fc09fc9e35ad0588313ea0fb44dee89dd
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-18 11:43:52 -0600

    Initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+            var x = 29;
+            var y = 55;
+
+            var answer = Add(x, y);
+
+            var a = 32.5m;
+            var b = 45.0m;
+
+            var decimalAnswer = Add(a, b);
+
+            var thirdAnswer = Add(1, 0, true);
+
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(thirdAnswer);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isTrue)
+        {
+            if(isTrue == true && x+y > 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (isTrue == true && x + y == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else if (isTrue == true && x + y < 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else
+                return (x + y).ToString();
+
         }
     }
 }
